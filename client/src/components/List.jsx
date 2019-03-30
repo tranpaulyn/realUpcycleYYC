971b1d30e2e22d400b18f65747eb705f7f9c0f32
Paulyn Tran <paulyntran@Paulyns-MacBook-Pro.local>
2019-03-30 12:32:15 -0600

    Ready for first push to heroku

---
+++ client/src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const List = ({list, onRemoveList=f=>f, editingList=f=>f}) =>
+    <div className="single-list" key={list.id}>
+        <h4>#{list.id} - {list.title}</h4>
+        <p>{list.excerpt}</p>
+        <button onClick={() => onRemoveList(list.id)}>Erase</button>
+        <button onClick={() => editingList(list.id)}>Edit</button>
+        <hr/>
+    </div>
+
+export default List;
