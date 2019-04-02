6783baf6662b72202e5db2c23c85840f814e5c76
arslan <arah09@hotmail.com>
2019-04-02 15:23:34 -0600

    completed branch

--- client/src/components/Learn.jsx
+++ client/src/components/Learn.jsx
@@ -9,7 +9,7 @@ class Home extends Component {
           <h1 className="App-title">This is the Learn route!</h1>
         </header>
         <div>
-
+          <a target='_blank' href='http://www.calgary.ca/UEP/WRS/Pages/Recycling-information/Residential-services/Recycling-depots/Recycling-Depots.aspx'>Find a bottle depot near you</a>
         </div>
       </div>
     );
