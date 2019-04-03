964d682640226378bf8e319a24318fab91cd6d12
Paulyn <paulyntran@live.ca>
2019-04-02 21:36:32 -0600

    Updated user waste item controller to add weight to ward

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import ListsContainer from './ListsContainer';
 import { Link } from 'react-router-dom';
+import { Button } from 'antd';
 
 class Home extends Component {
+  state = {
+    size: 'large',
+  };
   render() {
+    const size = this.state.size;
     return (
-      <div className="App">
-        <header className="App-header">
+      <div>
+        <header>
           <h1 className="App-title">Hello Hiplyst!</h1>
-            <Link className='signuplink' to={'/signup'}>Signup</Link>
-            <Link className='signuplink' to={'/dashboard'}>Login</Link>
+            <Link to={'/signup'}><Button type="primary" shape="round" size={size}>Sign Up</Button></Link>
+            <p/>
+            <Link to={'/dashboard'}><Button type="primary" shape="round" size={size} ghost>Login</Button></Link>
         </header>
-        <ListsContainer />
       </div>
     );
   }
