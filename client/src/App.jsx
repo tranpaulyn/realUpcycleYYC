2d5b93a7abc5d138616ff48a382640ca0793e58c
jatinderchima <jatinder.chima@gmail.com>
2019-04-04 17:40:14 -0600

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,21 +8,26 @@ import Dashboard from './components/Dashboard.jsx';
 import Learn from './components/Learn.jsx';
 import Dummy from './components/Dummy.jsx';
 import withStyles from './components/Footer.jsx';
+import { CSSTransition } from 'react-transition-group';
+import Quiz from './QuizApp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<Switch>
-  <Route exact path="/" component={Home} />
-  <Route exact path="/signup" component={Signup} />
-  <Route exact path="/add" component={Add} />
-  <Route exact path="/dashboard" component={Dashboard} />
-  <Route exact path="/Learn" component={Learn} />
-  {/* <Route exact path="/Navbar" component={withStyles} /> */}
-  <Route exact path="/Footer" component={withStyles} />
-  <Route exact path="/dummy" component={Dummy} />
-</Switch>
+        <CSSTransition>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/add" component={Add} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/Learn" component={Learn} />
+            {/* <Route exact path="/Navbar" component={withStyles} /> */}
+            <Route exact path="/Footer" component={withStyles} />
+            <Route exact path="/dummy" component={Dummy} />
+            <Route exact path="/quiz" component={Quiz} />
+          </Switch>
+        </CSSTransition>
       </div>
     );
   }
--- client/src/components/Learn.jsx
+++ client/src/components/Learn.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Footing from './Footer.jsx';
+import {Link} from 'react-router-dom'
 
 class Home extends Component {
   render() {
@@ -9,6 +10,8 @@ class Home extends Component {
         <header className="App-headers">
         </header>
         <div>
+          <a><Link to='/quiz'>Test your recycle knowledge! (QUIZ)</Link></a>
+          <div></div>
           <a target='_blank' href='http://www.calgary.ca/UEP/WRS/Pages/Recycling-information/Residential-services/Recycling-depots/Recycling-Depots.aspx'>Find a bottle depot near you</a>
           <div></div>
           <a target='_blank' href='http://www.calgary.ca/UEP/WRS/Pages/What-goes-where/Computers-and-computer-accessories.aspx'>Where to dispose of electronics</a>
