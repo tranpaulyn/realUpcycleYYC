ef5a4ae997903eda47ef5586633044aa8c4ad97a
Paulyn <paulyntran@live.ca>
2019-04-05 12:07:19 -0600

    Added fallback to application controller and routes.rb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home.jsx';
 import Signup from './components/Signup.jsx';
 import Add from './components/Add_item.jsx';
@@ -8,11 +8,14 @@ import Dashboard from './components/Dashboard.jsx';
 import Learn from './components/Learn.jsx';
 import withStyles from './components/Footer.jsx';
 import Leaderboard from './components/Leaderboard.jsx';
+import NotFound from './NotFound'
+import './components/components.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+
+      <Router>
 <Switch>
   <Route exact path="/" component={Home} />
   <Route exact path="/signup" component={Signup} />
@@ -21,8 +24,10 @@ class App extends Component {
   <Route exact path="/Learn" component={Learn} />
   <Route exact path="/Footer" component={withStyles} />
   <Route exact path="/Leaderboard" component={Leaderboard} />
+  <Route component={NotFound} />
 </Switch>
-      </div>
+</Router>
+
     );
   }
 }
