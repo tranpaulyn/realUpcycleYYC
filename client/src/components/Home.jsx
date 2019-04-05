2bfc5578abe7513af53e4f4a89ed4e701a04d75f
Paulyn <paulyntran@live.ca>
2019-04-05 12:48:26 -0600

    Adjusted alignment on the Home and SignUp Page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,12 +12,15 @@ class Home extends Component {
       
       <div className="home-bg">
         <header className="home">
+        <center>
           <h1 className="App-title">Upcycle</h1>
           <img alt="logo" className="hero-img" src="https://media0.giphy.com/media/etKPlfE85HW8udMMDl/giphy.gif"/>
             <p/>
+            
             <Link to={'/signup'}><button className="signup-btn">SIGN UP</button></Link>
             <p/>
             <Link to={'/dashboard'}><button className="login-btn">LOGIN</button></Link>
+            </center>
         </header>
       </div>
     );
